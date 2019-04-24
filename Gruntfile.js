9e3312f0886ffcec7e2ed94e712de45f8e6774e4
ProjectSoft <projectsoft2009@yandex.ru>
2019-04-24 23:16:33 +0400

    update

---
+++ Gruntfile.js
@@ -0,0 +1,149 @@
+module.exports = function(grunt){
+	require('load-grunt-tasks')(grunt);
+	require('time-grunt')(grunt);
+	var gc = {};
+	grunt.initConfig({
+		globalConfig : gc,
+		pkg : grunt.file.readJSON('package.json'),
+		imagemin: {
+			base: {
+				options: {
+					optimizationLevel: 3,
+					svgoPlugins: [
+						{
+							removeViewBox: false
+						}
+					]
+				},
+				files: [
+					{
+						expand: true,
+						flatten : true,
+						src: [
+							'src/images/*.{png,jpg,gif}'
+						],
+						dest: 'test/images/',
+						filter: 'isFile'
+					}
+				],
+			}
+		},
+		tinyimg: {
+			dynamic: {
+				files: [
+					{
+						expand: true,
+						cwd: 'test/images', 
+						src: ['**/*.{png,jpg,jpeg}'],
+						dest: 'docs/assets/images/'
+					}
+				]
+			}
+		},
+		less: {
+			main: {
+				options : {
+					compress: false,
+					ieCompat: false
+				},
+				files: {
+					'test/css/main.css': [
+						
+						'src/less/main.less'
+					],
+				}
+			}
+		},
+		autoprefixer:{
+			options: {
+				browsers: ["last 10 version"],//['Chrome > 70']
+				cascade: true
+			},
+			css: {
+				files: {
+					'tests/css/main.css' : ['test/css/main.css'],
+				}
+			},
+		},
+		group_css_media_queries: {
+			group: {
+				files: {
+					'tests/css/media.main.css': ['tests/css/main.css'],
+				}
+			}
+		},
+		cssmin: {
+			options: {
+				mergeIntoShorthands: false,
+				roundingPrecision: -1
+			},
+			minify: {
+				files: {
+					'docs/assets/css/main.css' : [
+						'tests/css/media.main.css'
+					],
+				}
+			}
+		},
+		pug: {
+			files: {
+				options: {
+					pretty: '\t',
+					separator:  '\n'
+				},
+				files: {
+					"docs/index.html": ['src/pug/index.pug'],
+				}
+			}
+		},
+		uglify : {
+			options: {
+				ASCIIOnly: true,
+			},
+			main: {
+				files: {
+					'test/js/main.js': [
+						'bower_components/jquery/dist/jquery.js',
+						'src/js/main.js'
+					]
+				}
+			},
+		},
+		bower_server: {
+			develop: {
+				options: {
+					//api: './mock',
+					port: 80,
+					base: 'docs/',
+					release: false
+				}
+			}
+		},
+		watch: {
+			options: {
+				livereload: true,
+			},
+			compile: {
+				files: [
+					'src/**/*.*'
+				],
+				tasks: [
+					'imagemin',
+					'tinyimg',
+					'less',
+					'autoprefixer',
+					'group_css_media_queries',
+					'cssmin',
+					'uglify',
+					'pug',
+				]
+			}
+		},
+	});
+	grunt.registerTask('default', [
+		'watch',
+	]);
+	grunt.registerTask('server', [
+		'bower_server',
+	]);
+}
