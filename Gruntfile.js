d8e9ff74bbded20b32ca539a338a1c3634c25db2
ProjectSoft <projectsoft2009@yandex.ru>
2024-06-19 00:28:27 +0400

    update

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,7 +8,12 @@ module.exports = function(grunt){
 	require('./modules/Downloader.js')(grunt);
 
 	var gc = {},
-		pkg = grunt.file.readJSON('package.json');
+		pkg = grunt.file.readJSON('package.json'),
+		path = require('path'),
+		uniqid = function () {
+			let result = URL.createObjectURL(new Blob([])).slice(-36).replace(/-/g, '');
+			return result;
+		};
 	function getTasks() {
 		switch(process.env.GRUNT_TASK){
 			case "develop":
@@ -128,7 +133,26 @@ module.exports = function(grunt){
 					// https://projectsoft-studionions.github.io/FontMassivePackInstaller/
 					data: function(dest, src) {
 						return {
-							"base": "https://projectsoft-studionions.github.io/FontMassivePackInstaller/",
+							"hash": uniqid(),
+							"repo": "projectsoft-studionions.github.io",
+							"userName": "ProjectSoft-STUDIONIONS",
+							"page": "FontMassivePackInstaller",
+							"download": "FontMassivePack.exe",
+							"title": "FontMassive Pack Installer | ProjectSoft GitHub Pages",
+							"h1title": "FontMassive Pack Installer",
+							"description": "Очень хороший набор программ. Особенно пользуюсь FontMassive и FonTemp, т. к. большую часть работаю с шаблонами, где используются сторонние шрифты, которых нет в системе.",
+							"keywords": "ProjectSoft, STUDIONIONS, ProjectSoft-STUDIONIONS,FontMassive Pack Installer, FontMassivePack",
+							"nickname": "ProjectSoft",
+							"logotype": "projectsoft.png",
+							"copyright": "2008 - all right reserved",
+							"open_graph": {
+								"image_16x9": "image16x9.jpg",
+								"image_16x9_width": "1920",
+								"image_16x9_height": "1080",
+								"image_1x1": "image1x1.jpg",
+								"image_1x1_width": "1080",
+								"image_1x1_height": "1080",
+							}
 						}
 					}
 				},
